dc73e15860e786efd67879a487916de362eb655b
Dugga Vamshidhar Reddy <207072251+duggavamshidhar@users.noreply.github.com>
2025-06-10 21:18:14 +0530

    feat: init

---
+++ src/main/java/com/duggavamshidhar/sharingan/SharinganApplication.java
@@ -0,0 +1,13 @@
+package com.duggavamshidhar.sharingan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SharinganApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SharinganApplication.class, args);
+	}
+
+}
